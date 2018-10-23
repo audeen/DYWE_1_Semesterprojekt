001a7de70ed9db818ec383ec143ebe08af3fdc93
r3817z <j.rebitz@posteo.de>
2018-10-23 21:58:15 +0200

    Kommentarblock weg

--- web/index.php
+++ web/index.php
@@ -1,13 +1,5 @@
 <?php
 
-//////////////////////////////////////////////////
-//  Übung 001    - blanko.php                   //
-//  Fachbereich Medien FH-Kiel - 3. Semester    //
-//  Beschreibung : Muster-Kommentarblock        //
-//  Ersteller    : Jannik Sievert               //
-//  Stand        : 20.09.2018                   //
-//  Version      : 1.0                          //
-//////////////////////////////////////////////////
 
 
 
